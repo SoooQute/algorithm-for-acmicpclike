245e7a59715d985ab235729fe075e23e402d7a36
SoooQute <172329849@qq.com>
2018-07-09 09:47:00 +0800

    工程文件

--- algorithm-for-acmicpclike.cpp
+++ algorithm-for-acmicpclike.cpp
@@ -2,10 +2,14 @@
 //
 
 #include "stdafx.h"
+#include "algorithms/RemainderGroup.h"
 
-
-int main()
-{
+int main() {
+    RemainderGroup rg(3);
+    rg.addEquation(3, 2);
+    rg.addEquation(5, 4);
+    rg.addEquation(7, 6);
+    cout << rg.getSolution() << endl;
     return 0;
 }
 
