c443a504c5d25c9c934fc8800fde98880b31cf29
SoooQute <172329849@qq.com>
2018-07-10 16:50:51 +0800

    快速幂

---
+++ algorithms/math/QuickPow.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "stdafx.h"
+using namespace std;
+inline long long pow(long long a, long long b, long long p) {
+    long long rtn = 1;
+    while (b) {
+        if (b & 1) rtn = rtn * a % p;
+        a = a * a % p;
+        b >>= 1;
+    }
+    return rtn;
+}
