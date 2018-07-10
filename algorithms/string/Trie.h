bd3ec147c3db2fb02da9a53258de327e7d6e84f7
SoooQute <172329849@qq.com>
2018-07-10 16:51:53 +0800

    Trie

---
+++ algorithms/string/Trie.h
@@ -0,0 +1,60 @@
+#pragma once
+#include "stdafx.h"
+using namespace std;
+class Trie {
+#define Trie_MAX_Letter_Num 26
+public:
+    Trie * next[Trie_MAX_Letter_Num];
+    Trie * father;
+    int cnt, mark;
+    Trie() {
+        cnt = 26;
+        memset(next, NULL, sizeof(next));
+        father = NULL;
+        mark = 0;
+    }
+    void reset() {
+        for (int i = 0; i < cnt; i++) {
+            if (next[i] != NULL) {
+                next[i]->reset();
+            }
+            delete next[i];
+        }
+        mark = false;
+    }
+    void Insert(char * ptr) {
+        Trie * root = this;
+        while (*ptr != '\0') {
+            if (root->next[(*ptr) - 'a'] == NULL) {
+                root->next[(*ptr) - 'a'] = new Trie;
+                (root->next[(*ptr) - 'a'])->father = root;
+            }
+            root = (root->next[(*ptr) - 'a']);
+            ptr++;
+        }
+        root->mark++;
+    }
+    bool Delete(char * ptr) {
+        Trie * root = this;
+        while (*ptr != '\0') {
+            if (root->next[(*ptr) - 'a'] == NULL) {
+                return false;
+            }
+            root = (root->next[(*ptr) - 'a']);
+            ptr++;
+        }
+        root->mark--;
+        return true;
+    }
+    Trie * Search(char * ptr) {
+        Trie * root = this;
+        while (*ptr != '\0') {
+            if (root->next[(*ptr) - 'a'] == NULL) {
+                return NULL;
+            }
+            root = (root->next[(*ptr) - 'a']);
+            ptr++;
+        }
+        return root;
+    }
+};
