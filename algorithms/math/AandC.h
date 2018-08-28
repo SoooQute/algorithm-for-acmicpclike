5b763f60549b776de70f4f2e98c08424edc0c879
SoooQute <172329849@qq.com>
2018-08-28 16:47:15 +0800

    排列组合

--- algorithms/math/AandC.h
+++ algorithms/math/AandC.h
@@ -7,15 +7,16 @@ public:
     long long mod;
     AandC(long long m, int n) {
         mod = m;
-        fac = (long long *)malloc((n) * sizeof(long long));
-        inv = (long long *)malloc((n) * sizeof(long long));
-        f = (long long *)malloc((n) * sizeof(long long));
+        fac = new long long[n + 5], inv = new long long[n + 5], f = new long long[n + 5];
         fac[0] = fac[1] = inv[0] = inv[1] = f[0] = f[1] = 1;
         for (int i = 2; i < n; i++) {
             fac[i] = fac[i - 1] * i % mod;
             f[i] = (mod - mod / i) * f[mod % i] % mod;
             inv[i] = inv[i - 1] * f[i] % mod;
         }
+    }
+    ~AandC() {
+        delete[] fac, delete[] inv, delete[] f;
     }
     //choose b from a
     long long A(int a, int b) {
@@ -24,4 +25,4 @@ public:
     long long C(int a, int b) {
         return fac[a] * inv[b] % mod * inv[a - b] % mod;
     }
-}; 
+};
